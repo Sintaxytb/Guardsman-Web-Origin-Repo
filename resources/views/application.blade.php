5fc5939cbc771abddca2e135acc855606a2b18c0
Sintaxytb <77858477+Sintaxytb@users.noreply.github.com>
2025-02-21 15:13:09 +0100

    Add files via upload

---
+++ resources/views/application.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" href="{{ asset('favicon.ico') }}" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Guardsman - Loading</title>
+  <link rel="stylesheet" type="text/css" href="{{ asset('loader.css') }}" />
+  @vite(['resources/ts/main.ts'])
+</head>
+
+<body>
+  <div id="app">
+    <div id="loading-bg">
+      <div class="loading-logo">
+        <img src="/logo_black.png" alt="Logo" />
+      </div>
+      <div class="loading">
+        <div class="effect-1 effects"></div>
+        <div class="effect-2 effects"></div>
+        <div class="effect-3 effects"></div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+    const loaderColor = '#FFFFFF'
+    const primaryColor = '#7367F0'
+
+    if (loaderColor)
+      document.documentElement.style.setProperty('--initial-loader-bg', loaderColor)
+
+    if (primaryColor)
+      document.documentElement.style.setProperty('--initial-loader-color', primaryColor)
+  </script>
+</body>
+
+</html>
